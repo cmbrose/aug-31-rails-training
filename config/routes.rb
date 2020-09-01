7c7aa31c7895b66766d49d7efe576fa7b4c90af8
Caleb Brose <5447118+cmbrose@users.noreply.github.com>
2020-09-01 23:29:12 +0000

    9/1 work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # add exactly /movies/:id route
+  # get '/movies/:id', to: 'movies#show', as: 'movie'
+
+  # add all default /movies routes
+  # resources :movies
+
+  # add the show action
+  resources :movies, only: [:show]
 end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -4,10 +4,16 @@ class MoviesSystemTest < ApplicationSystemTestCase
   test "visiting the show" do
     # As a user,
     # when I visit /movies/1
-    visit("/movies/1") # visit("/movies/1")
+    visit "/movies/1"
     # I see the title of the movie "Parasite"
-    assert_text("Parasite")
+    assert_text "Parasite"
     # I also see the name of the director "Bong Joon-ho"
     assert_text "Bong Joon-ho"
+  end
+
+  test "visiting for another movie" do
+    visit "/movies/2"
+    assert_text "Titanic"
+    assert_text "James Cameron"
   end
 end
